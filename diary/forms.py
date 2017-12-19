d2dc42826b2a3b6b9a1ae8cd86cc43b38566d972
mkhan62 <mkhan62@hawk.iit.edu>
2017-12-19 01:34:49 -0600

    forms created with widgets for css styling

---
+++ diary/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from diary import Chapter, Comment
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta():
+        model = Chapter
+        fields = ('title', 'text')
+
+    widgets = {
+        'title': forms.TextInput(attrs={'class': 'textinputclass'}),
+        'text' : forms.Textarea(attrs={'class': 'editable medium-editor-textarea postcontent'}),
+    }
+
+class CommentForm(forms.ModelForm):
+
+    class Meta():
+        model = Comment
+        fields = ('author', 'text')
+
+        widgets = {
+            'author': forms.TextInput(attrs={'class': 'textinputclass'}),
+            'text' : forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
+        }
